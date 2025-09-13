3bba997cff7fc29d225c10fe144ed8e338fbcb1a
Saifulislam8412sv <myth63305@gmail.com>
2025-09-13 14:54:53 +0530

    1.1

---
+++ absolutValue.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout<<"enter a number";
+    cin>>x;
+    if(x>0){
+        cout<<x;
+        cout<<endl;
+    }
+    else{
+        cout<<-x;
+    }
+}
