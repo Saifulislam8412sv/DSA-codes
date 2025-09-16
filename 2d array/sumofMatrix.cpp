597453855fa6fdcbc7b39d84ddfb9e6442ac2206
Saifulislam8412sv <myth63305@gmail.com>
2025-09-16 15:32:54 +0530

    1.1

---
+++ 2d array/sumofMatrix.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a1[3][3]={1,2,3,4,5,6,7,8,9};
+  int a2[3][3]={9,8,7,6,5,4,3,2,1};
+ 
+  for(int i=0;i<3;i++)
+  {
+     int sum[3][3];
+    for(int j=0;j<3;j++)
+    {
+        sum[i][j]=a1[i][j]+a2[i][j];
+        cout<<sum[i][j]<<" ";
+    }
+    cout<<endl;
+  }
+}
